0f0d9ba36c928b978f06e4e5d0e8c40ed9702a05
jonpecson <jonpecson.io@gmail.com>
2017-04-15 22:56:19 +0800

    initial commit

---
+++ src/components/progress-bar/progress-bar.module.ts
@@ -0,0 +1,16 @@
+// import { NgModule } from '@angular/core';
+// import { IonicModule } from 'ionic-angular';
+// import { ProgressBar } from './progress-bar';
+
+// @NgModule({
+//   declarations: [
+//     ProgressBar,
+//   ],
+//   imports: [
+//     IonicModule.forChild(ProgressBar),
+//   ],
+//   exports: [
+//     ProgressBar
+//   ]
+// })
+// export class ProgressBarModule {}
